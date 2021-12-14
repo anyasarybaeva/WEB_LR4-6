ff8a4af1fa6da95926250ef9726b077274373b18
Anna <annasarybaeva@MacBook-AirAnna.local>
2021-12-15 00:07:20 +0300

    delete new

--- dino_script.js
+++ dino_script.js
@@ -1,26 +1,40 @@
 const dino=document.getElementById("dino");
 const cactus=document.getElementById("cactus");
-const counter=document.getElementById("counter");
+const exit=document.getElementById("exit");
+const start=document.getElementById("start")
+const pause=document.getElementById("pause")
+const text=document.getElementById("text")
+
+
 let points=0;
-document.writeln("Очки:",points);
+text.textContent=points;
+exit.addEventListener('click', () => window.open('index.html'));
+start.addEventListener('click', () =>{
+    cactus.classList.add("move")
+    var startTime=Date.now();
+    
+});
+
 document.addEventListener("keydown",function(event){
     if(event.key==" ")
     {
         jump();
     }
-    else if(event.key=="Backspace")
-    {
-        window.location.href = 'index.html';
-    }
 });
+let isPoint = setInterval(function() {
+    let cactusleft=parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    text.textContent=points++;
+},1500)
 function jump()
 {
-    if(dino.classList!="jump")
+    if(dino.classList!="jump" && cactus.classList=="move")
     {
         dino.classList.add("jump")
+      
     }
     setTimeout(function(){
         dino.classList.remove("jump")
+
     },300)
 }
 let isAlive = setInterval(function() {
@@ -29,13 +43,15 @@ let isAlive = setInterval(function() {
     if(cactusleft<50 && cactusleft>0 && dinotop>=100)
     {
         alert("game over")
+        points=0;
     }
-    else if(cactusleft<50 && cactusleft>0 && dinotop<100)
-    {
-        counter.textContent=points++;
-    }
-},15)     
 
+    if(1495>=(Date.now()-startTime)>=1510 )
+    {
+        text.textContent=points++;
+        start=Date.now()
+    }
+},15)
 
 
 
